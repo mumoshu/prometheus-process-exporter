51a10042f62c676d6c2d5321bb713ab39baeb5ad
Yusuke Kuoka <ykuoka@gmail.com>
2019-09-11 21:45:41 +0900

    include 0.3.0 for repackaging

--- scripts/repackage-all.sh
+++ scripts/repackage-all.sh
@@ -4,7 +4,7 @@ set -e
 
 rm -rf packages/ || true
 mkdir -p packages/
-for v in 0.1.0 0.2.0 0.2.1 0.2.2; do
+for v in 0.1.0 0.2.0 0.2.1 0.2.2 0.3.0; do
   git checkout prometheus-process-exporter-$v
   helm package charts/prometheus-process-exporter --destination packages
 done
